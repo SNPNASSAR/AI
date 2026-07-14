232bf2b67482a9e1926f2a80d2a132ae413d40b9
Nassar <pashanassar@gmail.com>
2026-07-14 16:40:10 +0530

    1

--- simple_linear_reg.py
+++ simple_linear_reg.py
@@ -17,8 +17,11 @@ model.fit(customers, sales)
 # Streamlit UI
 st.markdown("""<p style="text-align:center;font-size:40px;font-weight:bold;color:#1f77b4;margin-bottom:20px;">Daily Cafe Sales</p>""",unsafe_allow_html=True)
 
-st.markdown("<h4> Problem statement: Build a linear regression model to predict sales for 60 customers.</h4>", unsafe_allow_html=True)
+st.markdown("<h4> Problem statement: Build a model to predict sales for 60 customers.</h4>", unsafe_allow_html=True)
             
+st.markdown("<h4> Model: A simple linear regression model is used in this scenario to predict sales for 60 customers.</h4>")
+
+
 
 # User input
 num_customers = st.number_input(
